2f35499384567d7db45b6ad5e838501f26ef4ac9
Michaelg199 <168252225+Michaelg199@users.noreply.github.com>
2025-01-28 16:42:35 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    int User_Entered_value = 0;
+    std::cout << "Hello World\n";
+    std::cout << "please enter a number:";
+    std::cin >> User_Entered_value;
+    std::cout << "\nUser_Entered_value was:";
+    std::cout << User_Entered_value;
+    return 0;
+}
